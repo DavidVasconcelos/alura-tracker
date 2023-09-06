3c7acece78cbf21258eca75d9de8b324c94971b5
David Vasconcelos <dvasconcelos@scale.com.br>
2023-09-05 21:06:37 -0300

    css

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <sidebar />
+    </div>
+    <div class="column is-three-quarter">
+      <taskform />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import SideBar from './components/SideBar.vue';
+import TaskForm from './components/TaskForm.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    'sidebar': SideBar,
+    'taskform': TaskForm
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
