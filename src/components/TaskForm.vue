ceafb170cb99758afe55bb4c60d7a3ad6c001ca3
David Vasconcelos <dvasconcelos@scale.com.br>
2023-09-05 21:51:00 -0300

    stopwatch

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -7,15 +7,15 @@
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>{{ timeElapsed }}</strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="initCount" :disabled="startIsDisabled">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="endCount" :disabled="stopIsDisabled">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -31,6 +31,35 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'TaskForm'
+    name: 'TaskForm',
+    data() {
+        return {
+            startIsDisabled: false,
+            stopIsDisabled: true,
+            timeInSeconds: 0,
+            stopWatch: 0
+        }
+    },
+    computed: {
+        timeElapsed(): string {
+            return new Date(this.timeInSeconds * 1000).toISOString().substring(11, 19)
+        }
+    },
+    methods: {
+        // 1 sec = 1000ms
+        initCount() {
+            this.stopWatch = setInterval(() => {
+                this.timeInSeconds += 1
+            }, 1000)
+            this.startIsDisabled = true
+            this.stopIsDisabled = false
+
+        },
+        endCount() {
+            this.startIsDisabled = false
+            this.stopIsDisabled = true
+            clearInterval(this.stopWatch)
+        }
+    },
 })
 </script>
